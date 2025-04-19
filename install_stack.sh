a1526f944d56d3ca734e4efce31bcd6ce680effa
Hasan OZER <hohoho21207+notification@gmail.com>
2025-04-19 15:43:38 +0300

    Update install_stack.sh

--- install_stack.sh
+++ install_stack.sh
@@ -5,9 +5,41 @@ if [[ "$EUID" -ne 0 ]]; then
   exit 1
 fi
 
+INSTALL_POSTGRES=false
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -psql|--postgres)
+      INSTALL_POSTGRES=true
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+sudo apt install gnupg2 wget
+sudo sh -c 'echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B7B3B788A8D3785C 
+sudo apt update
+sudo apt install -y postgresql-16 postgresql-contrib-16
+
+
 apt update && apt upgrade -y
-apt install -y curl wget net-tools dnsutils build-essential git gnupg lsb-release ca-certificates software-properties-common openssl nginx certbot python3-certbot-nginx uuid-runtime
+apt install -y curl gnupg2 wget net-tools dnsutils build-essential git gnupg lsb-release ca-certificates software-properties-common openssl nginx certbot python3-certbot-nginx uuid-runtime 
 curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && \
     apt install -y nodejs
+
+if [ "$INSTALL_POSTGRES" = true ]; then
+install -d /usr/share/postgresql-common/pgdg && \
+  curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
+
+. /etc/os-release && \
+  sudo sh -c "echo 'deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $VERSION_CODENAME-pgdg main' > /etc/apt/sources.list.d/pgdg.list" && \
+  sudo apt update && \
+  sudo apt -y install postgresql-16 postgresql-contrib-16
+fi
 
 ./nginx_default.sh
