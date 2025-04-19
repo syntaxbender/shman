52daa4c28ccf890cc07ba959aae7af2264a61bb0
Hasan OZER <hohoho21207+notification@gmail.com>
2025-04-19 05:41:15 +0300

    Update install_stack.sh

--- install_stack.sh
+++ install_stack.sh
@@ -9,3 +9,5 @@ apt update && apt upgrade -y
 apt install -y curl wget net-tools dnsutils build-essential git gnupg lsb-release ca-certificates software-properties-common openssl nginx certbot python3-certbot-nginx uuid-runtime
 curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && \
     apt install -y nodejs
+
+./nginx_default.sh
