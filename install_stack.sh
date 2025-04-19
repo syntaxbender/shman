beebf473c0c4b0a1f61ba8ec81a9c9d7b04a5b98
Hasan OZER <hohoho21207+notification@gmail.com>
2025-04-19 16:33:36 +0300

    Update install_stack.sh

--- install_stack.sh
+++ install_stack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DEBIAN_FRONTEND=noninteractive
 
 if [[ "$EUID" -ne 0 ]]; then
   echo "Please run as root! exiting..."
@@ -21,7 +22,9 @@ while [[ $# -gt 0 ]]; do
 done
 
 apt update && apt upgrade -y
+needrestart -r a
 apt install -y curl gnupg2 wget net-tools dnsutils build-essential git gnupg lsb-release ca-certificates software-properties-common openssl nginx certbot python3-certbot-nginx uuid-runtime 
+needrestart -r a
 wget -q https://deb.nodesource.com/setup_20.x -O ./nodesource.sh && \
 chmod +x ./nodesource.sh && \
 ./nodesource.sh && \
@@ -32,9 +35,9 @@ if [ "$INSTALL_POSTGRES" = true ]; then
 install -d /usr/share/postgresql-common/pgdg && \
   curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc && \
   . /etc/os-release && \
-  sudo sh -c "echo 'deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $VERSION_CODENAME-pgdg main' > /etc/apt/sources.list.d/pgdg.list" && \
-  sudo apt update && \
-  sudo apt -y install postgresql-16 postgresql-contrib-16
+  echo "deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $VERSION_CODENAME-pgdg main" > /etc/apt/sources.list.d/pgdg.list && \
+  apt update && \
+  apt -y install postgresql-16 postgresql-contrib-16
 fi
 
 ./nginx_default.sh
