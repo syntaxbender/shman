0638af0ce511122b9e4c5c362176450c151f7dc0
Hasan OZER <hohoho21207+notification@gmail.com>
2025-04-19 15:59:41 +0300

    Update install_stack.sh

--- install_stack.sh
+++ install_stack.sh
@@ -20,23 +20,18 @@ while [[ $# -gt 0 ]]; do
   shift
 done
 
-sudo apt install gnupg2 wget
-sudo sh -c 'echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B7B3B788A8D3785C 
-sudo apt update
-sudo apt install -y postgresql-16 postgresql-contrib-16
-
-
 apt update && apt upgrade -y
 apt install -y curl gnupg2 wget net-tools dnsutils build-essential git gnupg lsb-release ca-certificates software-properties-common openssl nginx certbot python3-certbot-nginx uuid-runtime 
-curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && \
-    apt install -y nodejs
+wget -q https://deb.nodesource.com/setup_20.x -O ./nodesource.sh && \
+chmod +x ./nodesource.sh && \
+./nodesource.sh && \
+apt install -y nodejs && \
+rm ./nodesource.sh
 
 if [ "$INSTALL_POSTGRES" = true ]; then
 install -d /usr/share/postgresql-common/pgdg && \
-  curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
-
-. /etc/os-release && \
+  curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc && \
+  . /etc/os-release && \
   sudo sh -c "echo 'deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $VERSION_CODENAME-pgdg main' > /etc/apt/sources.list.d/pgdg.list" && \
   sudo apt update && \
   sudo apt -y install postgresql-16 postgresql-contrib-16
